08b599f91e249d615a34935c3d7bc395b9e4f7bf
supriya <supriyachaudhari99@gmail.com>
2023-09-13 10:52:19 -0700

    Added test class

---
+++ src/com/velocity/demo/Test.java
@@ -0,0 +1,16 @@
+package com.velocity.demo;
+
+import java.util.ArrayList;
+
+
+public class Test {
+
+	public static void main(String[] args) 
+	{
+      ArrayList<String> al = new ArrayList<String>();
+      al.add("java");
+      al.add("hibernate");
+      System.out.println(al);
+
+}
+}
